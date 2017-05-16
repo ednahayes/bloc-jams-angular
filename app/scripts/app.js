116044dc543501e0ca3b0cd6e5bf1b80adc7a3a7
Edna Hayes <princessahe@hotmail.com>
2017-05-16 17:50:49 -0500

    checkpoint-5

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,6 +8,7 @@
         $stateProvider
             .state('landing', {
                 url: '/',
+                controller: 'LandingCtrl as landing',
                 templateUrl: '/templates/landing.html'
             })
             .state('album', {
@@ -16,8 +17,9 @@
             })
 
             .state('collection', {
-              url: '/collection',
-              templateUrl: '/templates/collection.html'
+                url: '/collection',
+                controller: 'CollectionCtrl as collection',
+                templateUrl: '/templates/collection.html'
             });
 
     }
